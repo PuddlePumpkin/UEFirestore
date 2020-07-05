37a349e8bd8b2e959a9bce0a13524f50d6f50729
KeiraTheCat <keira.k.exe@gmail.com>
2020-07-04 21:17:08 -0600

    json object stuff

--- Source/FireStore/Private/FSJsonObject.cpp
+++ Source/FireStore/Private/FSJsonObject.cpp
@@ -2,7 +2,7 @@
 
 
 #include "FSJsonObject.h"
-
+//sets
 void UFSJsonObject::SetStringField(FString fieldName, FString string)
 {
 	jobj->SetStringField(fieldName, string);
@@ -15,12 +15,48 @@ void UFSJsonObject::SetStringArrayField(FString fieldName, TArray<FString> strin
 		jsonStrArray.Add(MakeShareable(new FJsonValueString(Str)));
 	}
 	jobj->SetArrayField(fieldName, jsonStrArray);
-	//FString OutputString;
-	//TSharedRef< TJsonWriter<> > Writer = TJsonWriterFactory<>::Create(&OutputString);
-	//FJsonSerializer::Serialize(jobj.ToSharedRef(),Writer);
-	//UE_LOG(LogHttp, Display, TEXT("response: %s"), *OutputString);
 }
-//todo read stuff:
+void UFSJsonObject::SetFloatField(FString fieldName, float number)
+{
+	jobj->SetNumberField(fieldName, number);
+}
+void UFSJsonObject::SetBoolField(FString fieldName, bool boolean)
+{
+	jobj->SetBoolField(fieldName, boolean);
+}
+//gets
+FString UFSJsonObject::GetStringField(FString fieldName)
+{
+	return jobj->GetStringField(fieldName);
+}
+TArray<FString> UFSJsonObject::GetStringArrayField(FString fieldName)
+{
+	TArray<TSharedPtr<FJsonValue>> jsonStrArray;
+	jsonStrArray = jobj->GetArrayField(fieldName);
+	TArray<FString> StrArray;
+	for (TSharedPtr<FJsonValue> val : jsonStrArray)
+	{
+		StrArray.Add(val->AsString());
+	}
+	return StrArray;
+}
+float UFSJsonObject::GetFloatField(FString fieldName)
+{
+	return jobj->GetNumberField(fieldName);
+}
+int32 UFSJsonObject::GetIntegerField(FString fieldName)
+{
+	return jobj->GetIntegerField(fieldName);
+}
+bool UFSJsonObject::GetBoolField(FString fieldName)
+{
+	return jobj->GetBoolField(fieldName);
+}
+//util
+bool UFSJsonObject::HasField(FString fieldName)
+{
+	return jobj->HasField(fieldName);
+}
 FString UFSJsonObject::GetJsonString() 
 {
 	FString OutputString;
@@ -31,4 +67,16 @@ FString UFSJsonObject::GetJsonString()
 void UFSJsonObject::ClearJson()
 {
 	jobj = MakeShareable(new FJsonObject);
+}
+bool UFSJsonObject::SetJsonString(FString JString)
+{
+	TSharedRef<TJsonReader<>> reader = TJsonReaderFactory<>::Create(JString);
+	if (FJsonSerializer::Deserialize(reader, jobj))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
--- Source/FireStore/Public/FSJsonObject.h
+++ Source/FireStore/Public/FSJsonObject.h
@@ -20,9 +20,28 @@ class FIRESTORE_API UFSJsonObject : public UObject
 		void SetStringField(FString fieldName, FString string);
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set String Array Field", Tooltip = "Sets a string array field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
 		void SetStringArrayField(FString fieldName, TArray<FString> stringArray);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set Float Field", Tooltip = "Sets a float field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		void SetFloatField(FString fieldName, float number);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set Boolean Field", Tooltip = "Sets a boolean field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		void SetBoolField(FString fieldName, bool boolean);
+	//gets
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get String Field", Tooltip = "Gets a string field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		FString GetStringField(FString fieldName);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get String Array Field", Tooltip = "Gets a string array field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		TArray<FString> GetStringArrayField(FString fieldName);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Float Field", Tooltip = "Gets a float field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		float GetFloatField(FString fieldName);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Integer Field", Tooltip = "Gets a float field and converts it to an int.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		int32 GetIntegerField(FString fieldName);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Boolean Field", Tooltip = "Gets a boolean field.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		bool GetBoolField(FString fieldName);
 	//util
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Has Field", Tooltip = "Returns true if a field exists.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		bool HasField(FString fieldName);
 	UFUNCTION(BlueprintPure , meta = (DisplayName = "Get Json String", Tooltip = "Gets a string from json object.", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
 		FString GetJsonString();
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Clear Json", Tooltip = "Resets a json object to default", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
 		void ClearJson();
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set Json String", Tooltip = "Tries to set a json object to given json string", Keywords = "FireStore database datastore json"), Category = "Firestore Json")
+		bool SetJsonString(FString JString);
 };
